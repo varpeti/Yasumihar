3663c0593f4ac21a2fe2ee160498f878bdd8d754
varpeti <peti.var97@gmail.com>
2018-01-19 20:35:08 +0100

    player, cfg - mouse buttons

--- game/player.lua
+++ game/player.lua
@@ -7,40 +7,29 @@ player.r=0
 player.s=1
 player.speed=1000
 
-function player.update(dt)
-	for k,key in ipairs(cfg.keys.up) do
-		if love.keyboard.isDown(key) then
-			player.y=player.y-player.speed*dt
-			break -- Hogy ne tudjon dupla sebességgel menni
-		end 
-	end
-	for k,key in ipairs(cfg.keys.down) do
-		if love.keyboard.isDown(key) then
-			player.y=player.y+player.speed*dt
-			break
-		end 
-	end
-	for k,key in ipairs(cfg.keys.right) do
-		if love.keyboard.isDown(key) then
-			player.x=player.x+player.speed*dt
-			break
-		end 
-	end
-	for k,key in ipairs(cfg.keys.left) do
-		if love.keyboard.isDown(key) then
-			player.x=player.x-player.speed*dt
-			break
-		end 
+function bind(keys,fv)
+	for k,key in ipairs(keys) do
+		if love.keyboard.isDown(key) then --Billentyűzet gombok
+			fv()
+			return
+		end
+		local mkey = key:match("mouse(%d+)")
+		if mkey and love.mouse.isDown(tonumber(mkey)) then
+			fv()
+			return
+		end
 	end
 end
 
+function player.update(dt)
+	bind(cfg.keys.up,   function() player.y=player.y-player.speed*dt end)
+	bind(cfg.keys.down, function() player.y=player.y+player.speed*dt end)
+	bind(cfg.keys.right,function() player.x=player.x+player.speed*dt end)
+	bind(cfg.keys.left, function() player.x=player.x-player.speed*dt end)
+end
+
 function player.keypressed(key)
-	for k,key in ipairs(cfg.keys.escape) do
-		if love.keyboard.isDown(key) then
-			love.event.quit()
-			break
-		end 
-	end
+	bind(cfg.keys.escape,love.event.quit)
 	if cfg.cvar.debug3 then kiir.add("+"..key,3,{200,200,0}) end
 end
 
@@ -49,6 +38,7 @@ function player.keyreleased(key)
 end
 
 function player.mousepressed(x, y, button, istouch)
+	bind(cfg.keys.test, function() kiir.add("TEST",3,{200,200,0}) end)
     if cfg.cvar.debug3 then kiir.add("+mouse"..button,3,{200,200,0}) end
 end
 
